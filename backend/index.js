bb501e8cf5c9e5490ae914f57d52df82ec007fb6
RohanShirsath4 <rohanshirsath707@gmail.com>
2025-10-01 14:42:57 +0530

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,9 @@ const authRoutes = require('./routes/auth');
 const attendanceRoutes = require('./routes/attendence');
 const leaveRoutes = require('./routes/leave');
 const adminRoutes = require('./routes/admin');
-
+const path = require('path');
 const app = express();
+const _dirname  =path.resolve();
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +18,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/attendance', attendanceRoutes);
 app.use('/api/leave', leaveRoutes);
 app.use('/api/admin', adminRoutes);
+app.use(express.static(path.join(_dirname,"frontend/react/dist")));
+app.get((_, res) => {
+   res.sendFile(path.resolve(_dirname,"frontend/react","dist","index.html"));
+})
+
 const connectDB = async()=>{
  const data = await mongoose.connect(process.env.MONGODB_URI)
  if(data){
--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ats-system-68xk.onrender.com',
         changeOrigin: true,
         secure: false
       }
